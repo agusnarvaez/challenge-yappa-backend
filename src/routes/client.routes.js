789489f7de3a9cbca6c48e49b0ef8d4a0192fa54
agusnarvaez <agus.narvaez@outlook.com>
2024-01-18 08:59:24 -0300

    Implemento middlewares de validación

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -2,14 +2,16 @@
 import express from "express"
 const router = express.Router()
 
+// Validations
+import { validateGetById,validateCreate,validateUpdate } from "../middlewares/clientValidations.js"
+
 // Controllers
 import clientController from "../controllers/clientController.js"
 
 router.get('/',clientController.getAll)
-router.get('/:id',clientController.getById)
-router.get('/search/:name',clientController.searchOne)
-router.post('/',clientController.insert)
-router.patch('/:id',clientController.update)
-router.delete('/:id',clientController.delete)
+router.get('/:id',validateGetById,clientController.getById)
+router.post('/',validateCreate,clientController.insert)
+router.patch('/:id',validateGetById,validateUpdate, clientController.update)
+router.delete('/:id',validateGetById,clientController.delete)
 
 export default router
